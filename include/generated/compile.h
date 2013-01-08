5d58556d94bc92a28494288db2f8012d6ed700ba
eugene373 <eugene373@gmail.com>
2013-01-08 09:21:02 -0500

    GPU + CPU Overclock plus IOs and Govs also .57 patch update

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 13 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#13 SMP PREEMPT Tue Jan 8 00:48:26 EST 2013"
+#define LINUX_COMPILE_BY "eugene373"
+#define LINUX_COMPILE_HOST "eugene373"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
